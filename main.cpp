5310ef11503ee349b4fa98abcca1aa198309da2c
Mahmoud <mhablas4@gmail.com>
2026-03-11 15:58:37 -0700

    Added greet function on feature branch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void greet() {
+    cout << "This is a new feature!" << endl;
+}
+
 int main() {
     cout << "Hello, Git!" << endl;
     cout << "7OKA ragel boom" << endl;
+    greet();
     return 0;
 }
